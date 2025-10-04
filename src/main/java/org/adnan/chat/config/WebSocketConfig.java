868fe1b4eade499d7d04110dcf841f2489212c48
Adnan <jabalenuradnan6@gmail.com>
2025-10-04 22:31:42 +0600

    Allow all origins

--- src/main/java/org/adnan/chat/config/WebSocketConfig.java
+++ src/main/java/org/adnan/chat/config/WebSocketConfig.java
@@ -12,7 +12,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/wss").withSockJS();
+
+        registry.addEndpoint("/wss")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
